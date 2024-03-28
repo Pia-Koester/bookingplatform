821bebd5b0c943eeb0f395d910623d1ae3aa6330
Pia <pia.koester@hey.com>
2024-03-28 19:09:15 +0100

    feature: adds users to activities, removes them after cancelation, allows admins to edit the activities

--- routes/activities-routes.js
+++ routes/activities-routes.js
@@ -1,12 +1,24 @@
 const express = require("express");
-
+//TO DO: add authentication !!
 const {
   createActivity,
   getActivities,
+  getActivity,
+  registerUserForActivity,
+  unregisterUserFromActivity,
+  adminUpdateActivityDetails,
 } = require("../controllers/activities-controller.js");
 
 const activityRouter = express.Router();
 
 activityRouter.route("/").post(createActivity).get(getActivities);
+activityRouter
+  .route("/:activity_id")
+  .get(getActivity)
+  .put(registerUserForActivity);
+
+activityRouter.route("/:activity_id/cancel").put(unregisterUserFromActivity);
+
+activityRouter.route("/admin/:activity_id").put(adminUpdateActivityDetails); //TO DO: authenticate and authorize only admins
 
 module.exports = activityRouter;
