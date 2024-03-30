19e962b77c34b9af229216ddc731480a3ad7797d
Pia <pia.koester@hey.com>
2024-03-30 19:15:44 +0100

    featuer: setup authentification using jwt and preparing for saving token in local storage on client side

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/users-model.js");
 const asyncWrapper = require("../utils/asyncWrapper.js");
 
+// ********** Functions relating to the account, login and profile **************
 //create new user
 const createUser = asyncWrapper(async (req, res, next) => {
   const {
@@ -43,6 +44,69 @@ const createUser = asyncWrapper(async (req, res, next) => {
   res.status(201).json(user);
 });
 
+//user login
+const login = asyncWrapper(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email })
+    .select("+password")
+    .populate({
+      path: "registeredActivities",
+      // populate: { path: "instructor", model: "Instructor" },
+    })
+    .populate({
+      path: "activeMemberships",
+      populate: { path: "membershipPlan", model: "MembershipPlan" },
+    });
+
+  if (!user) {
+    throw new ErrorResponse("User does not exist!", 404);
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw new ErrorResponse("Incorrect Password!", 401);
+  }
+
+  const payload = {
+    id: user._id,
+    email: user.email,
+    firstName: user.firstName,
+    role: user.role,
+  };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "480m",
+  });
+
+  delete user.password;
+
+  res.json({ token, user });
+});
+
+//user logout
+//TO DO: token must be removed from local storage in frontend!!!
+const logout = asyncWrapper(async (req, res, next) => {
+  res.json({ success: true });
+});
+
+// user information / profile only available after login
+const getProfile = asyncWrapper(async (req, res, next) => {
+  const { id } = req.user;
+
+  const user = await User.findById(id)
+    .populate({
+      path: "registeredActivities",
+      // populate: { path: "instructor", model: "Instructor" },
+    })
+    .populate({
+      path: "activeMemberships",
+      populate: { path: "membershipPlan", model: "MembershipPlan" },
+    });
+  res.json(user);
+});
+
+// ********** Functions relating to booking and purchases **************
 //after booking a membership the users active memberships array needs to be filled
 const setUserMembership = asyncWrapper(async (req, res, next) => {
   const { _id, user } = req.userMembership;
@@ -60,4 +124,4 @@ const setUserMembership = asyncWrapper(async (req, res, next) => {
   res.json(membershipHolder.activeMemberships);
 });
 
-module.exports = { createUser, setUserMembership };
+module.exports = { createUser, setUserMembership, login, logout, getProfile };
