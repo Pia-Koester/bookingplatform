6b01f6287f7db3d9e4ac1e7c9ad90cc5539d0514
Pia <pia.koester@hey.com>
2024-04-05 15:56:44 +0200

    fix: add cors policy

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ const typeRouter = require("./routes/activityTypes-routes.js");
 const { connectDB } = require("./db.js");
 
 //Middlewares
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "200kb" }));
+
 //app.use(bodyParser.urlencoded({ extended: false })); // commented out due to test reasons
 
 app.get("/", (req, res) => {
